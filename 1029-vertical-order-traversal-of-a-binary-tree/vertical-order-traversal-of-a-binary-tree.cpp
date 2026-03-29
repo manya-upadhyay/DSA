589bd84000c14ed8cba2d6cb2e9b8f98209de88c
Manya Upadhyay <manyayuv8404@gmail.com>
2026-03-29 18:12:29 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (57.98%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ /*
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
@@ -41,5 +42,61 @@ public:
         nodes.emplace_back(col,row,node->val);
         dfs(node->left,row+1,col-1,nodes);
         dfs(node->right,row+1,col+1,nodes);
+    }
+};
+
+*/
+
+
+//second method
+
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,vector<int>>>nodes;
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        vector<vector<int>>ans;
+        if(root==NULL)
+        {
+            return ans;
+        }
+        q.push(make_pair(root,make_pair(0,0)));
+
+        while(!q.empty())
+        {
+            pair<TreeNode*,pair<int,int>>temp=q.front();
+            q.pop();
+            TreeNode*frontNode=temp.first;
+            int hd = temp.second.first;
+            int lvl = temp.second.second;
+
+            nodes[hd][lvl].push_back(frontNode->val);
+            if(frontNode->left)
+            {
+               q.push(make_pair(frontNode->left,make_pair(hd-1,lvl+1)));
+            }
+            if(frontNode->right)
+            {
+                q.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));
+            }
+        }
+
+      for(auto i : nodes)
+        {
+            vector<int> col;
+
+            for(auto j : i.second)
+            {
+                sort(j.second.begin(), j.second.end());
+
+                for(auto k : j.second)
+                {
+                    col.push_back(k);
+                }
+            }
+
+            ans.push_back(col);
+        }
+        return ans;
     }
 };
