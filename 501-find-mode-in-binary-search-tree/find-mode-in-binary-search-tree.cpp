7be58797f1ffa5606d41b5f3c4f7ea23ae519426
Manya Upadhyay <manyayuv8404@gmail.com>
2026-06-01 21:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (77.70%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -1,38 +1,41 @@
 class Solution {
-private:
-    TreeNode* prev = nullptr;
-    int curr_cnt = 0;
-    int max_cnt = 0;
-    vector<int> modes;
-
-    void inorder(TreeNode* root) {
-        if (!root) return;
-        inorder(root->left);
-        if (prev == nullptr) {
-            curr_cnt = 1;
-        } else if (root->val == prev->val) {
-            curr_cnt++;
-        } else {
-            curr_cnt = 1;
-        }
-        if (curr_cnt > max_cnt) {
-            max_cnt = curr_cnt;
-            modes = {root->val}; 
-        } else if (curr_cnt == max_cnt) {
-            modes.push_back(root->val); 
-        }
-        prev = root;
-        inorder(root->right);
-    }
-
 public:
+    int curr_freq=0;
+    int curr_Num=0;
+    int max_freq=0;
+    vector<int>res;
+
+    void dfs(TreeNode*root)
+    {
+        if(!root)
+        {
+            return;
+
+        }
+        dfs(root->left);
+        if(root->val==curr_Num)
+        {
+            curr_freq++;
+        }
+        else
+        {
+            curr_Num= root->val;
+            curr_freq=1;
+        }
+        if(curr_freq>max_freq)
+        {
+            res={};
+            max_freq = curr_freq;
+        }
+        if(curr_freq==max_freq)
+        {
+            res.push_back(root->val);
+        }
+        dfs(root->right);
+    }
     vector<int> findMode(TreeNode* root) {
-        prev = nullptr;
-        curr_cnt = 0;
-        max_cnt = 0;
-        modes.clear();
         
-        inorder(root);
-        return modes;
+        dfs(root);
+        return res;
     }
 };
